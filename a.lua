552eea21428f8a5b046bae5240461d962a645058
xsoma <73025329+xsoma@users.noreply.github.com>
2025-08-02 23:53:56 +0200

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,1 +1,21 @@
 -- we making a bank w this one
+
+-- TODO: make sure to add sanity checks here
+
+
+local psx_library = require(game.ReplicatedStorage:WaitForChild('Framework'):WaitForChild('Library'))
+if (psx_library) then
+	print("psx library found")
+end
+
+local get_my_banks = psx_library.Network.Invoke("get my banks")
+if (get_my_banks) then
+	print("get_my_banks found")
+end
+
+for i,v in pairs(psx_library.Save.Get().Pets) do
+	local pet_info = psx_library.Directory.Pets[v.id];
+	for x,y in pairs(pet_info) do
+		print(x,y);
+	end
+end
